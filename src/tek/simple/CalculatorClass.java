b7f56ad8a774a5ea926cf70c7b5b5a5fa0be08a0
massouda kohistani <massouda.s@live.com>
2022-07-28 18:12:18 -0400

    this is first hello world project

---
+++ src/tek/simple/CalculatorClass.java
@@ -0,0 +1,10 @@
+package tek.simple;
+
+public class CalculatorClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(6+7);
+	}
+
+}
---
+++ src/tek/simple/PrintMyName.java
@@ -0,0 +1,11 @@
+package tek.simple;
+
+public class PrintMyName {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Massouda Kohistani");
+		
+	}
+
+}
